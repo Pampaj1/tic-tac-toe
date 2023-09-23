e37baa5f780058e52cd1afb1e3fa66caa38149b0
Pampaj <dawidkawinski@opoczta.pl>
2023-09-23 19:44:54 +0200

    isnt work yet

--- src/main.js
+++ src/main.js
@@ -1,16 +1,36 @@
 const xClass = "x";
 const circleClass = "circle";
+const winningCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 const fieldElements = document.querySelectorAll('[data-field]');
 const board = document.getElementById('board');
+
+const winningPageTextElement = document.querySelector('winning-page');
 let circleTurn
 
 
-/* A function in which each field executes a "handleClick" function when clicked 
-funkcja, która nasłuchuje klikniecia na pole - wykonuje funkcje "handleClick" i moze byc uzyta raz na pole
-*/
-fieldElements.forEach(field => {
-    field.addEventListener('click', handleClick, { once: true})
-})
+startGame()
+
+function startGame () {
+    circleTurn = false;
+    
+    /* A function in which each field executes a "handleClick" function when clicked 
+    funkcja, która nasłuchuje klikniecia na pole - wykonuje funkcje "handleClick" i moze byc uzyta raz na pole
+    */
+    fieldElements.forEach(field => {
+        field.addEventListener('click', handleClick, { once: true})
+    })
+    setBoardHoverClass()
+}
+
 
 function handleClick(e) {
     const cell = e.target
@@ -18,16 +38,36 @@ function handleClick(e) {
     // Place mark
     placeMark(cell, currentClass)
     // Check for win
-    // Check for draw
-    // Switch turns
-    switchTurns()
+    if (checkWin(currentClass)) {
+        endGame(false)
+    } else if (isDraw()) { // Check for draw
+        endGame(true)
+    } else 
+    switchTurns() 
     setBoardHoverClass()
 }
 
+
+function endGame(draw) {
+    if (draw) {
+
+    } else {
+        winningPageTextElement.innerHTML = `${circleTurn ? "O" : "X"} Wygrywa!`
+    } 
+    winningPageTextElement.classList.add('show')
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(xClass) || cell.classList.contains(circleClass)
+    })
+}
+// Place mark
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
 
+// Switch turns
 function switchTurns() {
     circleTurn = !circleTurn
 }
@@ -40,4 +80,12 @@ function setBoardHoverClass() {
     } else {
         board.classList.add(xClass)
     }
+}
+
+function checkWin(currentClass) {
+    return winningCombination.some(combination => {
+        return combination.every(index => {
+            return fieldElements[index].classList.contains(currentClass)
+        })
+    })
 }
